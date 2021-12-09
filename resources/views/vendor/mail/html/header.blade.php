48d0951bb518e5cb91d792fd16aaf38f13949604
Gahasa Purba <gahasapurba.tech@gmail.com>
2021-12-09 20:16:05 +0700

    Fix Width Logo Email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Dog-Care-Indonesia')
-<img src="https://i.postimg.cc/50k93mjH/DCI.png" class="logo" alt="Dog Care Indonesia">
+<img src="https://i.postimg.cc/50k93mjH/DCI.png" width="300" alt="Dog Care Indonesia">
 @else
 {{ $slot }}
 @endif
